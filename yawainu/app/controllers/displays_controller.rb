efe272568f418c354c088f01dda4d188207eb7b2
yawainu <yanu@yawainu.com>
2016-05-21 23:18:51 +0900

    コントローラー作り

---
+++ yawainu/app/controllers/displays_controller.rb
@@ -0,0 +1,2 @@
+class DisplaysController < ApplicationController
+end
--- yawainu/app/helpers/application_helper.rb
+++ yawainu/app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  def default_meta_tags
+    {
+      title:       "Title",
+      description: "description",
+      keywords:    "Ruby,Meta,Tags",
+      viewport:    "width=device-width, initial-scale=1",
+      #icon: image_url("favicon.ico"),
+      noindex: ! Rails.env.production?,
+      charset: "UTF-8",
+      og: {
+        title: "title",
+        type: "website",
+        url: request.original_url,
+        #image: image_url(""),
+        site_name: "site name",
+        description: "description",
+        locale: "ja_JP"
+      }
+    }
+  end
+
 end
